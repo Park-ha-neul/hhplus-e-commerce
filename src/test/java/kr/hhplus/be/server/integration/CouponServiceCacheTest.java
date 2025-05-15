e4f152777c7945f447a3c1eb2fac84feb7c48961
Park-ha-nule <sodlfma4869@naver.com>
2025-05-15 21:30:22 +0900

    test: 선착순 쿠폰 발급 기능 Redis 연동 테스트코드 작성 - 중복 발급 예외발생 테스트 - 재고 감소 확인, 발급 사용자 Set 저장 및 조회 테스트 - ttl 설정 검증 테스트

--- src/test/java/kr/hhplus/be/server/integration/CouponServiceCacheTest.java
+++ src/test/java/kr/hhplus/be/server/integration/CouponServiceCacheTest.java
@@ -6,6 +6,7 @@ import kr.hhplus.be.server.domain.user.UserRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,12 +16,16 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDateTime;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.*;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 public class CouponServiceCacheTest {
+
+    @Autowired
+    private UserCouponService userCouponService;
 
     @Autowired
     private CouponService couponService;
@@ -35,6 +40,16 @@ public class CouponServiceCacheTest {
     private RedisTemplate<String, Object> redisTemplate;
 
     private static final Logger logger = LogManager.getLogger(CouponServiceCacheTest.class);
+
+    @BeforeEach
+    void setUp() {
+        redisTemplate.delete("coupon:1");
+        redisTemplate.delete("coupon:1:users");
+
+        for (int i = 0; i < 5; i++) {
+            redisTemplate.opsForList().rightPush("coupon:1", "1");
+        }
+    }
 
     @AfterEach
     void tearDown() {
@@ -68,5 +83,46 @@ public class CouponServiceCacheTest {
         Long redisListSize = redisTemplate.opsForList().size(redisKey);
         logger.info("Redis Len : {}", redisListSize);
         assertEquals(command.getTotalCount(), redisListSize);
+    }
+
+    @Test
+    void 쿠폰_정상_발급_테스트() {
+        Long userId = 1L;
+        Long couponId = 1L;
+
+        UserCouponResult result = userCouponService.issue(userId, couponId);
+
+        String stockListKey = "coupon:" + couponId;
+        Long redisListSize = redisTemplate.opsForList().size(stockListKey);
+        String issuedSetKey = "coupon:" + couponId + ":users";
+        Set<Object> members = redisTemplate.opsForSet().members(issuedSetKey);
+
+        logger.info("Redis List Key : {}", stockListKey);
+        logger.info("Redis List Key Len check : {}", redisListSize);
+        logger.info("Redis Set Key : {}", issuedSetKey);
+        logger.info("쿠폰 발급된 사용자 목록 : {}", members);
+
+        assertNotNull(result);
+        assertTrue(redisTemplate.opsForSet().isMember("coupon:1:users", String.valueOf(userId)));
+
+        Long ttlSeconds = redisTemplate.getExpire(issuedSetKey, TimeUnit.SECONDS);
+        logger.info("쿠폰 발급된 사용자 목록 TTL (초): {}", ttlSeconds);
+
+        // TTL이 0 이상이면 (만료 안 된 상태) 테스트 통과
+        assertNotNull(ttlSeconds);
+        assertTrue("TTL이 설정되어 있어야 합니다.", ttlSeconds > 0);
+    }
+
+    @Test
+    void 쿠폰_중복발급시_예외발생() {
+        Long userId = 1L;
+        Long couponId = 1L;
+        userCouponService.issue(userId, couponId);
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            userCouponService.issue(userId, couponId);
+        });
+
+        assertEquals("이미 발급된 쿠폰입니다.", exception.getMessage());
     }
 }
